25c3ab52c8803ff31e94526d20c7a27be1d8f07a
Eccirillo69 <ettore.cirillo.dev@gmail.com>
2023-05-24 23:53:43 +0200

    filtro + modifica generatore excel

--- BE/src/controllers/excel.ts
+++ BE/src/controllers/excel.ts
@@ -6,7 +6,11 @@ const prisma = new PrismaClient();
 export const excelPersonGenerator = async (req, res) => {
     try {
         // Recupera i dati dal database
-        const persons = await prisma.person.findMany();
+        const persons = await prisma.person.findMany({
+          where:{
+            role: "ADMIN"
+          }
+        });
 
         // Crea un nuovo workbook
         const wb = XLSX.utils.book_new();
@@ -37,8 +41,16 @@ export const excelPersonGenerator = async (req, res) => {
         // Crea un nuovo workbook
         const wb = XLSX.utils.book_new();
 
-        // Converte i dati in un worksheet
-        const ws = XLSX.utils.json_to_sheet(cars);
+        // Inizializza un nuovo worksheet
+        const ws = XLSX.utils.aoa_to_sheet([]);
+
+        // Aggiunge le intestazioni di colonna al worksheet
+        XLSX.utils.sheet_add_aoa(ws, [["ID", "LicensePlate", "Model", "RefersTo"]], {origin: 'A1'});
+
+        // Aggiunge i dati al worksheet
+        cars.forEach((car, index) => {
+            XLSX.utils.sheet_add_aoa(ws, [[car.id, car.plates, car.model, car.person_id]], {origin: `A${index + 2}`});
+        });
 
         // Aggiunge il worksheet al workbook
         XLSX.utils.book_append_sheet(wb, ws, "Cars");
@@ -53,7 +65,8 @@ export const excelPersonGenerator = async (req, res) => {
       console.error(error);
       res.status(500).json({ message: "Internal server error" });
     }
-  };
+};
+
 
   export const excelPointGenerator = async (req, res) => {
     try {
